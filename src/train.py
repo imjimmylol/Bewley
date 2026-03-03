a8f0f87abad445dcc6cab8669eed625f625b8833
jimmy <chenjinghe@chenjinghedeMacBook-Pro.local>
2026-03-03 18:42:45 +0800

    Add H-type, high v.s uncertain

--- src/train.py
+++ src/train.py
@@ -363,7 +363,8 @@ def train(config, run):
                     save_dir=os.path.join(base_checkpoint_dir, "decision_rules"),
                     log_to_wandb=True,
                     step=step,
-                    plots=["A1", "A1-1", "B1", "A1-1h"]
+                    plots=["A1", "A1-1", "B1", "A1-1h"],
+                    v_bar=main_state.v_bar.detach().cpu().numpy().flatten()
                 )
 
         # CRITICAL: Clear temporary variables to prevent memory leaks
